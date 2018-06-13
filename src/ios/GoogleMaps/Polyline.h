7c545e9f0c9ef57cbcc7246669e5f6e95e98572e
Lucian Mandoc <mandoclucian@yahoo.com>
2018-06-13 18:50:46 +0300

    Rename polyline class

--- src/ios/GoogleMaps/Polyline.h
+++ src/ios/GoogleMaps/Polyline.h
@@ -9,16 +9,16 @@
 #import "GoogleMaps.h"
 #import "MyPlgunProtocol.h"
 
-@interface PolylineGM : CDVPlugin<MyPlgunProtocol>
-@property (nonatomic, strong) GoogleMapsViewController* mapCtrl;
-- (void)createPolyline:(CDVInvokedUrlCommand*)command;
+@interface PolylineGM : CDVPlugin <MyPlgunProtocol>
+@property(nonatomic, strong) GoogleMapsViewController *mapCtrl;
+- (void)createPolyline:(CDVInvokedUrlCommand *)command;
 
-- (void)setColor:(CDVInvokedUrlCommand*)command;
-- (void)setWidth:(CDVInvokedUrlCommand*)command;
-- (void)setPoints:(CDVInvokedUrlCommand*)command;
-- (void)setZIndex:(CDVInvokedUrlCommand*)command;
-- (void)setVisible:(CDVInvokedUrlCommand*)command;
-- (void)remove:(CDVInvokedUrlCommand*)command;
-- (void)setGeodesic:(CDVInvokedUrlCommand*)command;
+- (void)setColor:(CDVInvokedUrlCommand *)command;
+- (void)setWidth:(CDVInvokedUrlCommand *)command;
+- (void)setPoints:(CDVInvokedUrlCommand *)command;
+- (void)setZIndex:(CDVInvokedUrlCommand *)command;
+- (void)setVisible:(CDVInvokedUrlCommand *)command;
+- (void)remove:(CDVInvokedUrlCommand *)command;
+- (void)setGeodesic:(CDVInvokedUrlCommand *)command;
 
 @end
